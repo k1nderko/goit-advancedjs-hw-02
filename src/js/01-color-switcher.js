224e3236ea72e531c505f027492a9c8c13f19ab9
k1nderko <107281239+k1nderko@users.noreply.github.com>
2023-12-05 23:52:45 +0300

    01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
 
+let intervalId;
+
+document.querySelector('[data-start]').addEventListener('click', startChangingColor);
+document.querySelector('[data-stop]').addEventListener('click', stopChangingColor);
+
+function startChangingColor() {
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  clearInterval(intervalId);
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChangingColor() {
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(intervalId);
+
+  document.body.style.backgroundColor = '';
+}
